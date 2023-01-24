40ca86ca610b3b382cd44f7d87223e02e51c0199
Gareth <garethshippen@yahoo.co.uk>
2023-01-24 16:42:53 +0000

    Size of the primes is definitely an issue

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws Exception
     {
-//        FindHighestPrime.find();
-        FileWriter fw = new FileWriter("src\\Results.csv");
-        BufferedWriter bw = new BufferedWriter(fw);
-
-        final int size = 100;
-        int[] results = new int[2];
-
-        for(int i = 0; i < size; i++)
+        for(int i = 0; i < 100; i++)
         {
-            results = FindHighestPrime.find();
-            bw.write(results[0] + ", " + results[1] + "\n");
-            System.out.println(i);
+            System.out.println(Tests.testKeys(151,157));
         }
-        bw.flush();
-        bw.close();
     }
 }
 
--- src/Tests.java
+++ src/Tests.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class FindHighestPrime
+public class Tests
 {
+    // ÿ = 11111111 in ascii
+    static final String testChar = "ÿ";
     public static int[] find() throws Exception {
-        // ÿ = 11111111 in ascii
-        final String testChar = "ÿ";
         boolean run = true;
         int[] results = new int[2];
         ArrayList<Integer> primes = new PrimeList().getPrimes();
@@ -33,5 +33,21 @@ public class FindHighestPrime
             }
         }
         return results;
+    }
+
+    public static boolean testKeys(int _p, int _q) throws Exception {
+        boolean theSame = false;
+        Keys keys;
+        keys = new Keys(_p, _q);
+        Encrypt encrypt = new Encrypt(keys.getE(), keys.getN());
+        Decrypt decrypt = new Decrypt(keys.getD(), keys.getN());
+
+        ArrayList<Long> cypher = encrypt.encrypt(testChar);
+        String message = decrypt.decrypt(cypher);
+        if(testChar.equals(message))
+        {
+            theSame = true;
+        }
+        return theSame;
     }
 }
